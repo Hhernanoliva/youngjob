81a61acf86b126f64abac9dfdf3fb621befbecac
Hernan Oliva <hhernanoliva@gmail.com>
2020-10-19 15:40:16 -0300

    cambios

--- js/plugin.js
+++ js/plugin.js
@@ -1,1 +1,14 @@
 import Bound from 'bound.js'
+
+const boundary = Bound({
+    margins: { bottom: -400 }
+});
+
+const h4 = document.querySelector("h4");
+
+boundary.watch(h4, () => {
+    h4.classList.add('h4rev');
+    console.log("ASLDJLASDJLAS ")
+}, () => {
+    h4.classList.remove('h4rev')
+});
